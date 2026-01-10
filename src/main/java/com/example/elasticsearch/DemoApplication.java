978ae0f8f1fca76ee0cfadd2a9c7c847aceeaf12
Shanmukha Katuri <shanmukha.k@gmail.com>
2026-01-11 01:19:19 +0400

    Scool: added the elasticsearch repository alignment

--- src/main/java/com/example/elasticsearch/DemoApplication.java
+++ src/main/java/com/example/elasticsearch/DemoApplication.java
@@ -2,11 +2,13 @@ package com.example.elasticsearch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode;
 
 @SpringBootApplication
 @EnableSpringDataWebSupport(pageSerializationMode = PageSerializationMode.VIA_DTO)
+@EnableElasticsearchRepositories
 public class DemoApplication {
 
 	public static void main(String[] args) {
